697212fe0329269c0845c83407b35754a56fa3cd
vidyant Sharma <vidyantkumar5519@gmail.com>
2025-03-28 11:43:58 +0530

    diffrent

--- interviewQue/Array/ReverseArray.java
+++ interviewQue/Array/ReverseArray.java
@@ -1,20 +1,25 @@
+
 import java.util.*;
 
-public class ReverseArray {
-   [] public ReverseArray(int[] arr) { 
-        int l = 0, r = arr.length - 1;
+class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        new ReverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public ReverseArray(int[] arr) {
+        int l = 0;
+        int r = arr.length - 1;
         while (l < r) {
-            int temp = arr[l];
+            int temp = arr[l]; // Fixed: Correct array access
             arr[l] = arr[r];
             arr[r] = temp;
             l++;
             r--;
         }
-    }
-    
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        new ReverseArray(arr);  
-        System.out.println(Arrays.toString(arr));
+
     }
 }
